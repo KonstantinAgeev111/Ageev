4d540b73c683bb6cacba94c63946e6facc0a580c
KonstantinAgeev111 <73308835+KonstantinAgeev111@users.noreply.github.com>
2024-01-15 19:52:11 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            double firstfigure = Convert.ToDouble(a);
+
+            string b = Console.ReadLine();
+
+            double secondfigure = Convert.ToDouble(b);
+
+            string c = Console.ReadLine();
+
+            double thirdfigure = Convert.ToDouble(c);
+
+            double sum = firstfigure + secondfigure + thirdfigure;
+
+            double multiplication = firstfigure * secondfigure * thirdfigure;
+
+            Console.WriteLine("Cумма трех чисел: " + sum);
+            Console.WriteLine("Произведение трех чисел: " + multiplication);
+            Console.WriteLine("Произведение трех чисел: " + multiplication);
+
+        }
+    }
+}
+
+
+   
